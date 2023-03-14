dd54441615d1366e2c830f0ff8d5b07c90900f69
krezoiv <erick.adan@gmail.com>
2023-03-14 15:11:35 -0600

    14.03

--- src/api/routes/negocio.js
+++ src/api/routes/negocio.js
@@ -4,16 +4,43 @@ const router = express.Router();
 
 const mysqlConnection = require('../connection/connection');
 
-router.get('/', (req, res)=>{
+/*router.get('/', (req, res)=>{
    
     mysqlConnection.query('CALL listar_negocio',
      (err, rows, fields)=>{
         if(!err){
-            res.json((rows));
+            res.json(rows);
         }else{
             console.log(err)
         }
     })
+})*/
+
+/**
+ * 
+ server.get("/api/student", (req, res) => {
+    var sql = "SELECT * FROM student";
+    db.query(sql, function (error, result) {
+      if (error) {
+        console.log("Error Connecting to DB");
+      } else {
+        res.send({ status: true, data: result });
+      }
+    });
+  });
+*/
+router.get('/', (req, res)=> {
+    var sql = "SELECT * FROM negocio";
+    mysqlConnection.query(sql, function(error, result){
+        if(error){
+            console.log("error en conexion");
+        }else{
+            res.send(result)
+        }
+    })
+
 })
 module.exports = router;
-//test
+
+ 
+ 
--- src/api/routes/ventas.js
+++ src/api/routes/ventas.js
@@ -5,9 +5,9 @@ const router = express.Router();
 const mysqlConnection = require('../connection/connection');
 
 router.post('/', (req, res)=>{
-    const {fecha, ventas_libreria, ventas_tienda, ventas_impresiones, ventas_refa } = req.body;
-    mysqlConnection.query('CALL ventas_diarias(?,?,?,?,?)',
-    [fecha, ventas_libreria, ventas_tienda, ventas_impresiones, ventas_refa ], (err, rows, fields)=>{
+    const {fecha } = req.body;
+    mysqlConnection.query('CALL nueva_venta(?)',
+    [fecha], (err, rows, fields)=>{
         if(!err){
             res.json(rows);
         }else{
