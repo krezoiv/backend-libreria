629c7d96ddb0374e678cb57cdedabe82f56af960
krezoiv <erick.adan@gmail.com>
2023-04-04 22:05:06 -0600

    04.04

--- src/api/routes/ventas.js
+++ src/api/routes/ventas.js
@@ -15,5 +15,47 @@ router.post('/', (req, res)=>{
         }
     })
 })
+
+
+router.post('/', (req, res)=> {
+    const {fecha } = req.body;
+    var sql = "CALL nueva_venta(?)";
+    mysqlConnection.query(sql, [fecha], function(error, result){
+        if(error){
+            console.log("error en conexion");
+        }else{
+            res.json({status: false, message: "Aperturado" } )
+        }
+    })
+})
+
+router.post('/detalleVenta', (req, res) => {
+    const {id_negocio, monto} = req.body;
+    var sql = 'CALL detalleventa(?,?)';
+    mysqlConnection.query(sql, [id_negocio, monto], function(error, result){
+        if(error){
+            console.log("error en conexion");
+        }else{
+            res.json({status: true, message: "Agregado" } )
+        }
+    })
+})
+
+router.get('/lastId', (req, res)=> {
+    var sql = "SELECT idVentas FROM ventas ORDER BY idVentas DESC LIMIT 1;";
+    mysqlConnection.query(sql, function(error, result){
+        if(error){
+            console.log("error en conexion");
+        }else{
+            res.send(result)
+        }
+    })
+
+});
+
+
+
+
+
 module.exports = router;
 //test
