10654b4934e900f28729f174838e7e5b855c464e
krezoiv <erick.adan@gmail.com>
2023-04-12 21:48:47 -0600

    12.04

--- src/api/routes/ventas.js
+++ src/api/routes/ventas.js
@@ -41,6 +41,20 @@ router.post('/detalleVenta', (req, res) => {
     })
 })
 
+router.post ('/listaDetVenta', (req, res) => {
+    const {fecha} = req.body;
+    //var sql = 'CALL mostrarVenta(?)';
+    var sql = 'SELECT dv.monto, vt.fecha,ng.negocio from detalle_venta dv INNER JOIN ventas vt ON dv.idventas = vt.idventas INNER JOIN negocio ng ON ng.id_negocio = dv.id_negocio WHERE vt.fecha = ?;'
+    mysqlConnection.query(sql, [fecha], function(error, result){
+        if(error){
+            console.log('error en conexion');
+        }else{
+            
+            res.send(result)
+        }
+    })
+})
+
 router.get('/lastId', (req, res)=> {
     var sql = "SELECT idVentas FROM ventas ORDER BY idVentas DESC LIMIT 1;";
     mysqlConnection.query(sql, function(error, result){
